b79d89c4cdd420be3f459208a36ab5f32ad322bc
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2026-01-22 15:54:18 +0200

    feat(header): add website header

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import logo from "@/assets/icons/logo.svg";
+import moonIcon from "@/assets/icons/moon-icon.svg";
+import avatarImg from "@/assets/images/image-avatar.jpg";
+
+const Header = () => {
+  return (
+    <header className="relative xl:min-w-25.75">
+      <div
+        className="
+        bg-header flex justify-between xl:rounded-tr-lg xl:rounded-br-lg
+        xl:h-screen xl:flex-col xl:fixed xl:top-0 xl:left-0">
+        <div className="logo-brand-shape aspect-square">
+          <img
+            className="z-1 w-[clamp(28px,4vw,40px)] aspect-square"
+            src={logo}
+            alt="Website logo shape"
+          />
+        </div>
+        <div className="flex xl:flex-col">
+          <button className="cursor-pointer px-[clamp(24px,3vw,42px)] xl:py-8">
+            <img src={moonIcon} alt="Moon image" />
+          </button>
+          <div className="px-[clamp(24px,3vw,32px)] py-5 border-[#494e6e] max-xl:border-l xl:border-t ">
+            <img
+              className="w-[clamp(32px,4vw,40px)] aspect-square rounded-full"
+              src={avatarImg}
+              alt="Profile image"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
