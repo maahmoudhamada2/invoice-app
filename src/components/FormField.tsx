f8b67211e034b0638fc99fcc8689d382b9444a26
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2026-02-26 00:22:22 +0200

    feat(form): add FormField shared component

---
+++ src/components/FormField.tsx
@@ -0,0 +1,31 @@
+interface FormFieldProps {
+  container: {
+    size: string;
+  };
+  label: {
+    text: string;
+  };
+  input: {
+    id: string;
+    type: string;
+    disabled: boolean;
+  };
+}
+
+const FormField = ({ container, label, input }: FormFieldProps) => {
+  return (
+    <div className={`relative flex flex-col gap-y-2.25 ${container.size}`}>
+      <label className="text-body text-muted" htmlFor={input.id}>
+        {label.text}
+      </label>
+      <input
+        className="text-main text-body-bold tracking-[-0.25px] px-5 pt-4.5 pb-3.75 not-disabled:border-2 border-input-outline rounded-xs disabled:text-subtle"
+        id={input.id}
+        type={input.type}
+        disabled={input.disabled}
+      />
+    </div>
+  );
+};
+
+export default FormField;
