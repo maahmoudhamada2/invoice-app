81a3d322d3d41d48fc250857dd1fcb836305e24c
maahmoudhamada <maahmoud.hamada@hotmail.com>
2026-04-28 04:47:05 +0300

    feat(invoices): replace mock data with dynamic user-created invoices

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import InvoiceForm from "./features/invoices/InvoiceForm.view";
+import InvoicePage from "./features/invoices/InvoicePage.view";
 
 const App = () => {
   return (
     <div className="flex flex-col xl:flex-row">
       <Header />
       <main className="relative">
+        <InvoicePage />
         <InvoiceForm />
       </main>
     </div>
