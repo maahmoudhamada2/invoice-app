69a9fdc929d582c4f54229306f2422db7fe6df6e
maahmoudhamada <maahmoud.hamada@hotmail.com>
2026-04-30 17:38:37 +0300

    feat(invoice-status): add dynamic invoices counter to status bar

--- src/features/invoices/components/StatusBar.tsx
+++ src/features/invoices/components/StatusBar.tsx
@@ -2,16 +2,43 @@ import invoiceButtonsConfig from "../config/invoiceButtons.config";
 import Button from "../../../components/Button/Button";
 import Filters from "./Filters";
 import useAppUiStore from "@/store/useAppUiStore";
+import useInvoicesStore from "../store/useInvoicesStore";
+
+const invoiceCounter = (status: string, invCount: number) => {
+  const isAll = status === "all";
+  const mainText =
+    invCount === 0
+      ? `No ${!isAll ? status : ""} invoices`
+      : `There ${invCount === 1 ? "is" : "are"} ${invCount} 
+      ${isAll ? "total" : ""} 
+      ${!isAll ? status : ""}
+      invoice${invCount > 1 ? "s" : ""}`;
+
+  const altText = `
+    ${invCount === 0 ? "No" : invCount}
+    ${!isAll ? status : ""}
+    invoice${invCount > 1 || invCount === 0 ? "s" : ""}`;
+  return { mainText, altText };
+};
 
 const StatusBar = () => {
   const openForm = useAppUiStore((state) => state.openForm);
+  const invoices = useInvoicesStore((state) => state.invoices);
+  const filterStatus = useInvoicesStore((state) => state.filterStatus);
+
+  const filteredInvoices = invoices.filter((invoice) =>
+    filterStatus === "all" ? true : invoice.status === filterStatus,
+  );
+  const invCount = filteredInvoices.length;
+  const { mainText, altText } = invoiceCounter(filterStatus, invCount);
+
   return (
     <div className="flex justify-between">
       <div className="flex flex-col gap-0.75 md:gap-1.5">
         <h1 className="text-heading text-main">Invoices</h1>
         <p className="text-subtle dark:text-muted text-body">
-          <span className="md:hidden"> 7 invoices</span>
-          <span className="max-md:hidden">There are 7 total invoices</span>
+          <span className="max-md:hidden">{mainText}</span>
+          <span className="md:hidden">{altText}</span>
         </p>
       </div>
       <div className="flex items-center gap-10">
